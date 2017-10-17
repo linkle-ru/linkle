330221fd899e79c42bc184b7f80232730adfafba
Semyon Fomin <taxnuke@gmail.com>
2017-10-17 12:34:25 +0300

    добавляет выделенный обработчик ошибок для API

--- api/routers.js
+++ api/routers.js
@@ -10,4 +10,15 @@ router.get('/goto/:alias', controllers.goto);
 router.route('/aliases')
     .post(controllers.newAlias);
 
+// Обработчик ошибок API
+router.use((err, req, res, next) => {
+    // Кидаем ошибку только в окружении development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // Рендерим страницу с ошибкой
+    res.status(err.status || 500);
+    res.json(err.message);
+});
+
 module.exports = router;
