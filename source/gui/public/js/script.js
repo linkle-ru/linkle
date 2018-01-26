a348e02ab75aa5677a3e9a2a6989348e01187cab
Semyon Fomin <taxnuke@gmail.com>
2018-01-26 18:46:15 +0300

    улучшает ux, добавляет плагины для валидации и подсказок

---
+++ source/gui/public/js/script.js
@@ -0,0 +1,7 @@
+$('#nigger').popover({
+  trigger:'hover',
+  placement:'left',
+  container: 'body',
+  content: 'Допускаются только латинские и русские буквы, цифры, дефис, нижнее подчеркивание',
+  title: 'Ограничения'
+});
