a9cb1c8dfd4c79c77c78d999e05213dc1a583786
Semyon Fomin <taxnuke@gmail.com>
2018-11-16 22:11:56 +0300

    убирает блок watch из конфига pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,6 @@ module.exports = {
   apps: [{
     name: 'URL-ShortenerAPI',
     script: './source/server.js',
-    watch: [
-      './source/api/v1/*',
-      './source/lib/*',
-      './source/models/*',
-      './source/app.js',
-      './source/server.js'
-    ],
     env: {
       NODE_ENV: 'development',
       DEBUG: 'url-short:*'
