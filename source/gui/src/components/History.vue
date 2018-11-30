69c8dd7efb16c38eb4406f651b483ad3a7f9b858
Semyon Fomin <taxnuke@gmail.com>
2018-11-30 14:01:46 +0300

    улучшает ux

--- source/gui/src/components/History.vue
+++ source/gui/src/components/History.vue
@@ -17,82 +17,76 @@
         <v-card>
           <v-list two-line>
             <template v-for="(link, index) in source">
-              <v-hover
+              <v-list-tile
                 :key="link.name"
+                avatar
+                @click="followLink(link)"
               >
-                <v-list-tile
-                  slot-scope="{ hover }"
-                  avatar
-                  @click="followLink(link)"
-                >
-                  <v-list-tile-avatar>
-                    <v-layout
-                      align-center
-                      justify-center
-                      column
-                      fill-height
-                    >
-                      <v-tooltip bottom>
-                        <v-flex
-                          slot="activator"
-                          class="body-2"
-                        >
-                          {{ link | visitsFormatter }}
-                        </v-flex>
-                        <span>бета</span>
-                      </v-tooltip>
-                      <v-icon
-                        small
-                        color="grey lighten-1"
+                <v-list-tile-avatar>
+                  <v-layout
+                    align-center
+                    justify-center
+                    column
+                    fill-height
+                  >
+                    <v-tooltip top>
+                      <v-flex
+                        slot="activator"
+                        class="body-2"
                       >
-                        visibility
-                      </v-icon>
+                        {{ link | visitsFormatter }}
+                      </v-flex>
+                      <span>Переходы по ссылке</span>
+                    </v-tooltip>
+                    <v-icon
+                      small
+                      color="grey lighten-1"
+                    >
+                      visibility
+                    </v-icon>
 
-                    </v-layout>
-                  </v-list-tile-avatar>
+                  </v-layout>
+                </v-list-tile-avatar>
 
-                  <v-list-tile-content>
-                    <v-list-tile-title>
-                      {{ $sanitize(link.title || "Без заголовка") }}
-                    </v-list-tile-title>
-                    <v-list-tile-sub-title>
-                      <a
-                        class="subheading"
-                        onclick="return false"
-                        :href="link.name"
-                      >{{ link.name }}</a>
-                      &mdash;
-                      <span class="font-weight-thin">{{ link.href }}</span>
-                    </v-list-tile-sub-title>
-                  </v-list-tile-content>
-                  <v-list-tile-action v-show="hover">
-                    <v-btn
-                      icon
-                      ripple
-                      @click.stop="cbCopy(`${shared.origin}/${link.name}`)"
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ $sanitize(link.title || "Без заголовка") }}
+                  </v-list-tile-title>
+                  <v-list-tile-sub-title>
+                    <a
+                      class="subheading"
+                      onclick="return false"
+                      :href="link.name"
+                    >{{ link.name }}</a>
+                    &mdash;
+                    <span class="font-weight-thin">{{ link.href }}</span>
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-btn
+                    icon
+                    ripple
+                    @click.stop="cbCopy(`${shared.origin}/${link.name}`)"
+                  >
+                    <v-icon
+                      color="blue lighten-2"
                     >
-                      <v-icon
-                        color="blue lighten-2"
-                      >
-                        file_copy
-                      </v-icon>
-                    </v-btn>
-                  </v-list-tile-action>
-                  <v-list-tile-action v-show="hover">
-                    <v-btn
-                      icon
-                      ripple
-                      @click.stop="deleteLink(link)"
+                      file_copy
+                    </v-icon>
+                  </v-btn>
+                  <v-btn
+                    icon
+                    ripple
+                    @click.stop="deleteLink(link)"
+                  >
+                    <v-icon
+                      color="red lighten-2"
                     >
-                      <v-icon
-                        color="red lighten-2"
-                      >
-                        remove_circle
-                      </v-icon>
-                    </v-btn>
-                  </v-list-tile-action>
-                </v-list-tile>
-              </v-hover>
+                      remove_circle
+                    </v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
               <v-divider
                 v-if="index + 1 < source.length"
                 :key="index"
@@ -157,7 +151,7 @@ export default {
     },
     deleteLink(link) {
       const index = this.source.indexOf(link)
-      confirm('Вы уверены?') && this.source.splice(index, 1)
+      confirm('Вы уверены? Это действие необратимо!') && this.source.splice(index, 1)
     },
     cbCopy
   }
